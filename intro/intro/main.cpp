f2aa4aa9ba007392dfd6c36fffd8dd73e3ff34d3
chaotingchong-crypto <chaotingchong@gmail.com>
2025-09-23 10:34:36 +0800

    first

---
+++ intro/intro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  intro
+//
+//  Created by Eric  CC on 2025/9/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
